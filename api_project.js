dcd54755e646f054af453d0d63ce0c952ecca2ec
nitin095 <nitinshokeen9@gmail.com>
2018-12-10 12:57:13 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "toDo",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-10T07:22:17.612Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
