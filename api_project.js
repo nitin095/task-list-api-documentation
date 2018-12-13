7d528cea19d2dca96d4e17ba8afe1dc96e29a8bd
nitin095 <nitinshokeen9@gmail.com>
2018-12-13 15:09:13 +0530

    updated

--- api_project.js
+++ api_project.js
@@ -1,13 +1,15 @@
 define({
-  "name": "toDo",
+  "name": "Task List",
   "version": "1.0.0",
-  "description": "",
+  "description": "Task-List API Documentation",
+  "title": "Task Pad APIs",
+  "url": "http://ec2-13-23z3-92-229.ap-south-1.compute.amazonaws.com/api",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-10T07:22:17.612Z",
+    "time": "2018-12-13T09:38:10.738Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
